e4a1023708475be036a5c5d04f99c7d116a9a3e5
grJDh <rusmukhametov@gmail.com>
2023-04-07 18:36:42 +0300

    Added slider, input, sorting, and checkbox
    - A slider for selecting the maximum time
    - An input field for manually entering the maximum time
    - Cards sorting after filtering
    - A checkbox for reversing the sort order

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { DateTime } from "luxon";
 import "./App.css";
 
 import Card from "./components/Card/Card";
@@ -6,6 +7,45 @@ import Card from "./components/Card/Card";
 import subsData from "./response.json";
 
 const App = () => {
+  const filterData = data => {
+    const filteredData = data.filter(element => {
+      const uploadDate = DateTime.fromISO(element.lastVideoDate);
+      const difference = uploadDate.diffNow("days").toObject();
+      return Math.abs(difference.days) > maxTime;
+    });
+
+    return filteredData;
+  };
+
+  const sortData = data => {
+    const sortedData = data.sort((a, b) => {
+      const aDate = DateTime.fromISO(a.lastVideoDate);
+      const bDate = DateTime.fromISO(b.lastVideoDate);
+
+      const dateDifference = aDate.diff(bDate, "days").toObject();
+
+      return dateDifference.days;
+    });
+
+    return sortedData;
+  };
+
+  const [maxTime, setMaxTime] = useState(180);
+  const [filteredAndSortedData, setFilteredAndSortedData] = useState(filterData(subsData));
+  const [isAscending, setIsAscending] = useState(true);
+
+  useEffect(() => {
+    const debounceDelay = setTimeout(() => {
+      setFilteredAndSortedData(sortData(filterData(subsData)));
+    }, 1000);
+
+    return () => clearTimeout(debounceDelay);
+  }, [maxTime]);
+
+  // useEffect(() => {
+  //   setFilteredData(filterData(subsData));
+  // }, [maxTime]);
+
   // let myHeaders = new Headers();
   // myHeaders.append("Content-Type", "application/json");
 
@@ -24,12 +64,37 @@ const App = () => {
   //TODO: replace flex with grid in App
   return (
     <div className="App">
-      {subsData.map((data, i) => (
-        <Card
-          data={data}
-          key={i}
+      <div className="controls">
+        <input
+          type="range"
+          min="0"
+          max="180"
+          value={maxTime}
+          onChange={e => setMaxTime(e.target.value)}
         />
-      ))}
+        <input
+          min={0}
+          type="number"
+          value={maxTime}
+          onChange={e => setMaxTime(e.target.value)}
+        />
+        <label>
+          <input
+            type="checkbox"
+            checked={isAscending}
+            onChange={() => setIsAscending(prevValue => !prevValue)}
+          />
+          <p>Reverse order</p>
+        </label>
+      </div>
+      <div className="card-grid">
+        {(isAscending ? filteredAndSortedData.slice().reverse() : filteredAndSortedData).map((data, i) => (
+          <Card
+            data={data}
+            key={i}
+          />
+        ))}
+      </div>
     </div>
   );
 };
