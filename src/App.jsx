0ce2c62bc93120a42b2640a85052b62a456217b6
grJDh <rusmukhametov@gmail.com>
2023-04-07 19:40:27 +0300

    Finished slider, number input and dropdown functionality.
    CSS will come later.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,29 @@ import "./App.css";
 import Card from "./components/Card/Card";
 
 import subsData from "./response.json";
+import Slider from "./components/Slider/Slider";
 
 const App = () => {
+  const [filteredAndSortedData, setFilteredAndSortedData] = useState([]);
+  const [isAscending, setIsAscending] = useState(true);
+  const [numberValue, setNumberValue] = useState(6);
+  const [dropdownValue, setDropdownValue] = useState("month(s)");
+
   const filterData = data => {
     const filteredData = data.filter(element => {
       const uploadDate = DateTime.fromISO(element.lastVideoDate);
-      const difference = uploadDate.diffNow("days").toObject();
-      return Math.abs(difference.days) > maxTime;
+      let difference = {};
+      switch (dropdownValue) {
+        case "week(s)":
+          difference = uploadDate.diffNow("days").toObject();
+          return Math.abs(difference.days) >= numberValue * 7;
+        case "year(s)":
+          difference = uploadDate.diffNow("years").toObject();
+          return Math.abs(difference.years) >= numberValue;
+        default:
+          difference = uploadDate.diffNow("months").toObject();
+          return Math.abs(difference.months) >= numberValue;
+      }
     });
 
     return filteredData;
@@ -30,9 +46,31 @@ const App = () => {
     return sortedData;
   };
 
-  const [maxTime, setMaxTime] = useState(180);
-  const [filteredAndSortedData, setFilteredAndSortedData] = useState(filterData(subsData));
-  const [isAscending, setIsAscending] = useState(true);
+  const sliderSteps = [
+    "1 week(s)",
+    "2 week(s)",
+    "3 week(s)",
+    "4 week(s)",
+    "1 month(s)",
+    "2 month(s)",
+    "3 month(s)",
+    "4 month(s)",
+    "5 month(s)",
+    "6 month(s)",
+    "7 month(s)",
+    "8 month(s)",
+    "9 month(s)",
+    "10 month(s)",
+    "11 month(s)",
+    "12 month(s)",
+    "1 year(s)",
+    "2 year(s)",
+    "3 year(s)",
+    "4 year(s)",
+    "5 year(s)",
+  ];
+
+  const dateOptions = ["week(s)", "month(s)", "year(s)"];
 
   useEffect(() => {
     const debounceDelay = setTimeout(() => {
@@ -40,11 +78,24 @@ const App = () => {
     }, 1000);
 
     return () => clearTimeout(debounceDelay);
-  }, [maxTime]);
+  }, [numberValue, dropdownValue]);
+
+  const handleSetNumberValue = value => {
+    const sliderValue = sliderSteps[value];
+    const numInputValue = parseInt(sliderValue.substring(0, sliderValue.indexOf(" ")));
+
+    let dropdownValue = "month(s)";
+    if (sliderValue.includes("year(s)")) dropdownValue = "year(s)";
+    else if (sliderValue.includes("week(s)")) dropdownValue = "week(s)";
+    else dropdownValue = "month(s)";
+
+    setNumberValue(numInputValue);
+    setDropdownValue(dropdownValue);
+  };
 
   // useEffect(() => {
   //   setFilteredData(filterData(subsData));
-  // }, [maxTime]);
+  // }, [numberValue]);
 
   // let myHeaders = new Headers();
   // myHeaders.append("Content-Type", "application/json");
@@ -61,23 +112,35 @@ const App = () => {
   //     .then(response => console.log(JSON.stringify(response)));
   // }, []);
 
-  //TODO: replace flex with grid in App
+  console.log(numberValue.toString() + " " + dropdownValue);
+
   return (
     <div className="App">
       <div className="controls">
-        <input
-          type="range"
-          min="0"
-          max="180"
-          value={maxTime}
-          onChange={e => setMaxTime(e.target.value)}
+        <Slider
+          handleSetNumberValue={handleSetNumberValue}
+          // numberValue={numberValue}
+          currentStep={sliderSteps.indexOf(numberValue.toString() + " " + dropdownValue)}
         />
         <input
           min={0}
           type="number"
-          value={maxTime}
-          onChange={e => setMaxTime(e.target.value)}
+          value={numberValue}
+          onChange={e => setNumberValue(e.target.value)}
         />
+        <select
+          onChange={e => setDropdownValue(e.target.value)}
+          value={dropdownValue}
+        >
+          {dateOptions.map((option, i) => (
+            <option
+              key={i}
+              value={option}
+            >
+              {option}
+            </option>
+          ))}
+        </select>
         <label>
           <input
             type="checkbox"
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,17 @@
+const Slider = ({ currentStep, handleSetNumberValue }) => {
+  // console.log(currentStep);
+  return (
+    <div>
+      <input
+        type="range"
+        min={0}
+        max={20}
+        step={1}
+        onChange={e => handleSetNumberValue(e.target.value)}
+        value={currentStep}
+      />
+    </div>
+  );
+};
+
+export default Slider;
