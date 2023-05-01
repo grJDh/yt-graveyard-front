d3af83c88f63f64cf324a3ff70f6d9e23ffd9564
grJDh <rusmukhametov@gmail.com>
2023-05-01 23:42:13 +0300

    Testing js vh for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import "./App.css";
 
 import Title from "./components/Title/Title";
 import Loading from "./components/Loading/Loading";
+import { useEffect } from "react";
 const App = () => {
   //username cannot be empty or
+
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
 
   return (
     <div className="App">
       <Title />
 
-      {/* <Loading text="Walking to graveyard..." /> */}
+      <Loading text="Walking to graveyard..." />
 
-      <Outlet />
+      {/* <Outlet /> */}
     </div>
   );
 };
